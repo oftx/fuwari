2f569c943452dcf790f267398ce1bcbc66439908
oftx <52040558+oftx@users.noreply.github.com>
2025-08-17 01:34:52 +0800

    Update favicon

--- src/config.ts
+++ src/config.ts
@@ -31,11 +31,46 @@ export const siteConfig: SiteConfig = {
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		{
+		  src: '/favicon/favicon-light-32.png',    // Path of the favicon, relative to the /public directory
+		  theme: 'light',                          // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		  sizes: '32x32',                          // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		},
+		{
+		  src: '/favicon/favicon-dark-32.png',
+		  theme: 'dark',
+		  sizes: '32x32',
+		},
+		{
+		  src: '/favicon/favicon-light-128.png',
+		  theme: 'light',
+		  sizes: '128x128',
+		},
+		{
+		  src: '/favicon/favicon-dark-128.png',
+		  theme: 'dark',
+		  sizes: '128x128',
+		},
+		{
+		  src: '/favicon/favicon-light-180.png',
+		  theme: 'light',
+		  sizes: '180x180',
+		},
+		{
+		  src: '/favicon/favicon-dark-180.png',
+		  theme: 'dark',
+		  sizes: '180x180',
+		},
+		{
+		  src: '/favicon/favicon-light-192.png',
+		  theme: 'light',
+		  sizes: '192x192',
+		},
+		{
+		  src: '/favicon/favicon-dark-192.png',
+		  theme: 'dark',
+		  sizes: '192x192',
+		},
 	],
 };
 
