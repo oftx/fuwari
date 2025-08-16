43db3a72f53e97e0c06612d5d30b7e55dd881204
oftx <52040558+oftx@users.noreply.github.com>
2025-08-16 19:46:17 +0800

    Update domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.oftx.top/",
+	site: "https://0f.nz/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
